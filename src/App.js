3096cc1a9d049d90e7ebaed71b8ee38f6420afcd
handy-o <dyhan9420@gmail.com>
2022-05-21 11:58:34 +0900

    뮤직플레이어 프로젝트 기초 셋팅

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Footer from './Footer';
+import Section from './Section';
+import './scss/style.scss';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Section />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+  return (
+    <footer>
+      <p>2022 DCODELAB &copy; ALL RIGHTS RESERVED.</p>
+    </footer>
+  )
+}
+
+export default Footer;
